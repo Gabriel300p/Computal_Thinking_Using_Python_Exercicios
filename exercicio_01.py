08cdac5910e041763caf9544dd79bbe94d6efab7
Gabriel300p <gabriel.andrade200p@gmail.com>
2021-03-19 15:09:03 -0300

    Initial commit

---
+++ exercicio_01.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Entre com o primeiro número: "))
+numero2 = int(input("Entre com o segundo número: "))
+numero3 = int(input("Entre com o primeiro número: "))
+
+soma = numero1 + numero2 + numero3
+soma2 = soma * soma
+
+print("O quadrado da soma é: ", soma2)
---
+++ exercicio_02.py
@@ -0,0 +1,9 @@
+numero1 = int(input("Entre com o primeiro número: "))
+numero2 = int(input("Entre com o segundo número: "))
+numero3 = int(input("Entre com o terceiro número: "))
+numero4 = int(input("Entre com o quarto número: "))
+
+soma1 = numero1 * numero3
+soma2 = numero2 + numero4
+
+print("A multiplicação da o resultado de: ", soma1, "e a adição: ", soma2)
